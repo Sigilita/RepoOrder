230095985557917213a3d39b64847603c6712b17
Nasser <nasser.gonzalez@gmail.com>
2013-03-06 19:51:53 +0100

    GPS conectado

--- src/com/nasserysergio/orderit/MainActivity.java
+++ src/com/nasserysergio/orderit/MainActivity.java
@@ -6,7 +6,13 @@ import java.io.InputStreamReader;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.audiofx.BassBoost.Settings;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,6 +24,11 @@ public class MainActivity extends Activity implements OnClickListener
 {
 	Button btnValidar;
 	EditText editText;
+	private LocationManager locManager;
+	private LocationListener locListener;
+	private double lat;
+	private double lon;
+	private boolean isGps=false;;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
@@ -29,6 +40,101 @@ public class MainActivity extends Activity implements OnClickListener
 		editText = (EditText)findViewById(R.id.editText1);
 		
 		btnValidar.setOnClickListener(this); 
+		
+		locManager= (LocationManager) getSystemService(LOCATION_SERVICE);
+		isGps=locManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		if(!isGps){
+			activarGps();
+		}else{
+			isGps=true;
+		}
+		
+		Location lastLoc=locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		if(lastLoc!=null){
+			lat=lastLoc.getLatitude();
+			lon=lastLoc.getLongitude();
+		}
+		
+		locListener = new LocationListener() {
+			
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+				
+				
+			}
+			
+			@Override
+			public void onProviderEnabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProviderDisabled(String provider) {
+				
+				
+			}
+			
+			@Override
+			public void onLocationChanged(Location location) {
+				cambioPosicion(location);
+				
+			}
+		};
+		locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 20000,0,locListener);
+		
+		
+	}
+	public void activarGps(){
+		Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+		startActivity(intent);
+		/*AlertDialog.Builder alerta= new AlertDialog.Builder(getApplicationContext());
+		alerta.setTitle("Encendiendo GPS");
+		alerta.setMessage("El GPS no esta conectado. Conectelo Ahora.");
+		alerta.setPositiveButton("Configuracion", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+				startActivity(intent);
+			}
+		});
+		/*AlertDialog.Builder alertDialog = new AlertDialog.Builder(getApplicationContext());
+		 
+        // Setting Dialog Title
+        alertDialog.setTitle("GPS is settings");
+ 
+        // Setting Dialog Message
+        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+ 
+        // Setting Icon to Dialog
+        //alertDialog.setIcon(R.drawable.delete);
+ 
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int which) {
+                Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                getApplicationContext().startActivity(intent);
+            }
+        });
+ 
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            dialog.cancel();
+            }
+        });*/
+       /* alerta.show();*/
+	}
+	public void cambioPosicion(Location loc)
+	{
+		if(loc!=null){
+			lat=loc.getLatitude();
+			lon=loc.getLongitude();
+		}else{
+			lat=locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude();
+			lon=locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude();
+		}
 	}
 	
 	public void ejecutarActivityProductos()
@@ -36,10 +142,16 @@ public class MainActivity extends Activity implements OnClickListener
 		Intent intent = new Intent(this, ActivityProductos.class);
         startActivity(intent);
 	}
+	private void isGpsEnabled(){
+		isGps=locManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+	}
+	
 
 	@Override
 	public void onClick(View v) 
 	{
+		isGpsEnabled();
+		if(isGps){
 		if (comprobarCodigo(editText.getText().toString()))
 		{
 			ejecutarActivityProductos();
@@ -47,6 +159,11 @@ public class MainActivity extends Activity implements OnClickListener
 		}
 		else
 			mostrarToast("Código introducido no válido");
+		}else{
+			mostrarToast("Ha de conectar el gps");
+			activarGps();
+		}
+		
 	}
 	
 	@SuppressLint("ShowToast")
@@ -75,4 +192,5 @@ public class MainActivity extends Activity implements OnClickListener
 			return false;
 		}	
 	}
+	
 }
